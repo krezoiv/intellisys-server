d89d1f629f8f8795fc04df98563ee543e9fe2e59
krezoiv <erick.adan@gmail.com>
2023-10-21 13:17:19 -0600

    21oct23 alerta de mensaje roles

--- src/controllers/employees/role.controllers.js
+++ src/controllers/employees/role.controllers.js
@@ -3,7 +3,7 @@ import { req, res } from "express";
 import RoleModel from "../../models/role.model";
 import RolesFieldsMappings from "../../mapping/roleMapping";
 import { roles_queries } from "../../database/querys/rolesQuerys";
-
+import sql from "mssql";
 
 export const createRole = async (req, res) => {
   const roleModel = new RoleModel(req.body);
@@ -14,21 +14,25 @@ export const createRole = async (req, res) => {
 
     const rolesMapping = RolesFieldsMappings.getMappings();
 
-    for (const fieldsdRole in rolesMapping) {
-      request.input(
-        fieldsdRole,
-        rolesMapping[fieldsdRole],
-        roleModel[fieldsdRole]
-      );
+    for (const fieldsRole in rolesMapping) {
+      request.input(fieldsRole, rolesMapping[fieldsRole], roleModel[fieldsRole]);
     }
 
     await request.query(roles_queries.createNewRole);
+    res.json(roleModel);
 
-    res.json(roleModel)
   } catch (error) {
+    if (error.originalError) {
+      // Si hay un error original, muestra el mensaje personalizado
+      const errorMessage = error.originalError.message || "Error al crear el rol";
+      console.error("Error al crear el rol:", error);
 
-    console.log(error)
+      res.status(500).json({ error: errorMessage });
+    } else {
+      // Muestra un mensaje de error genérico en caso de otro tipo de error
+      const errorMessage = error.message || "Error al crear el rol";
+      console.error("Error al crear el rol:", error);
+      res.status(500).json({ error: errorMessage });
+    }
   }
-
-  
 };
